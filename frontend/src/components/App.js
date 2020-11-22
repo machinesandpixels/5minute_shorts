2b83404dd00489db4c47db0e3bb829d96c439acc
JQ <2016quezadaj@gmail.com>
2020-11-21 21:20:28 -0800

    Created Editor component and installed Codemirror

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Editor from './Editor';
 
 const App = () => {
   return (
     <div>
       <h1>  Dev Sandbox </h1>
+      <div className="pane top-pane">
+        <Editor></Editor>
+        <Editor></Editor>
+        <Editor></Editor>
+      </div>
+      <div className="pane">
+        <iframe
+         title="output"
+         sandbox="allow-scripts"
+         frameborder="0"
+         width="100%"
+         height="100%">
+        </iframe>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/Editor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'codemirror/lib/codemirror.css';
+
+const Editor = () => {
+    return (
+        <div>
+            Editor
+        </div>
+    )
+}
+
+export default Editor;
