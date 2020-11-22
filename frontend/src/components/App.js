639b43eb23ac265acf5b650d07cceefa4f8f5ac2
JQ <2016quezadaj@gmail.com>
2020-11-21 23:49:00 -0800

    Set state to Editor component and now syntax highlighting is working

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../layout/Header';
 import Editor from './Editor';
 
 const App = () => {
+
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [js, setJs] = useState('');
+
   return (
     <div>
      <Header />
       <div className="pane top-pane">
-        <Editor></Editor>
-        <Editor></Editor>
-        <Editor></Editor>
+        <Editor 
+          title='HTML'
+          value={html}
+          language='xml'
+          onChange={setHtml}
+          />
+          <Editor 
+          title='CSS'
+          value={css}
+          language='css'
+          onChange={setCss}
+          />
+          <Editor 
+          title='JavaScript'
+          value={js}
+          language='javascript'
+          onChange={setJs}
+          />
       </div>
       <div className="pane">
         <iframe
