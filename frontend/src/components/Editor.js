01f3c75212e207502adb6db7e846a204efa85790
JQ <2016quezadaj@gmail.com>
2020-11-24 00:45:03 -0800

    Added open and close icons to Editor component

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'codemirror/theme/yonce.css';
@@ -6,18 +6,33 @@ import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/css/css';
 import 'codemirror/mode/javascript/javascript';
 import { Controlled } from 'react-codemirror2';
+import { 
+    BsArrowsAngleExpand, 
+    BsArrowsAngleContract 
+} from "react-icons/bs";
 
 const Editor = ({ title, value, language, onChange }) => {
+
+    const [open, setOpen] = useState(true);
 
     const handleChange = (editor, data, value) => {
         onChange(value)
     }
 
     return (
-        <div className="editor--container">
+        <div 
+            className={`editor--container
+            ${open ? '' : 'collapsed'}`}
+        >
             <div className="editor-title">
                 { title }
-                <button>X</button>
+                <button
+                    className="expand-btn"
+                    type="button" 
+                    onClick={() => setOpen(prevOpen => ! prevOpen)}
+                >
+                { open ? <BsArrowsAngleExpand  /> : <BsArrowsAngleContract /> }
+                </button>
             </div>
             <Controlled 
                 onBeforeChange={handleChange}
